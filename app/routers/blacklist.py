3246c1c6956ce48b0b9a70ec66c046b80615eca1
calylo <calylo@pm.me>
2024-01-27 15:56:05 +0100

    add input validation to query parameters

--- app/routers/blacklist.py
+++ app/routers/blacklist.py
@@ -1,11 +1,15 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Query
 
 from app.models.blacklist import postBody, main as blacklistModel
 
 router = APIRouter()
 
 @router.get("/fibonacci/blacklist")
-def read_root(page: int | None = 0, pageSize: int | None = 100):
+def read_root(
+    page: Annotated[int | None, Query(title="page number to get", ge=0)] = 0, 
+    pageSize: Annotated[int | None, Query(title="page number to get", ge=1)] = 100
+):
     return blacklistModel.getPage(page, pageSize)
 
 
--- app/routers/number.py
+++ app/routers/number.py
@@ -1,11 +1,15 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Query
 
 from app.models.fibonacci import main as fibonacciModel
 
 router = APIRouter()
 
 @router.get("/fibonacci/number")
-def read_root(page: int | None = 0, pageSize: int | None = 100):
+def read_root(
+    page: Annotated[int | None, Query(title="page number to get", ge=0)] = 0, 
+    pageSize: Annotated[int | None, Query(title="page number to get", ge=1)] = 100
+):
     return fibonacciModel.getPage(page, pageSize)
 
 
