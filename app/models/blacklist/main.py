35f1a856009ce9b4c976d490cf625e07b38b8d3f
calylo <calylo@pm.me>
2024-01-27 14:25:01 +0100

    prevent duplicate records in blacklist

--- app/models/blacklist/main.py
+++ app/models/blacklist/main.py
@@ -6,5 +6,8 @@ _blacklist = []
 def addToList(n):
     if(not FibonacciLib.isFibonacciNumber(n)):
         raise HTTPException(status_code=400, detail="Number is not in fibonacci sequence")
+    if(n in _blacklist):
+        raise HTTPException(status_code=409, detail="Number is already in blacklist")
     _blacklist.append(n)
+
     return {"result": n}
